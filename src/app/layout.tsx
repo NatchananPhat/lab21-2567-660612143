9ad5a7d66ce8cc0d3fddbb497b4e52b5c7c45350
NatchananP <natchananpinky@gmail.com>
2024-10-05 12:58:38 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,11 +34,11 @@ export default function RootLayout({
     } else {
       //check if token is still valid
       try {
-        const resp = await axios.get("/api/user/checkAuthen", {
+        await axios.get("/api/user/checkAuthen", {
           headers: { Authorization: `Bearer ${token}` },
         });
         $authenStore.set({ token, authenUsername });
-      } catch (err) {
+      } catch (err: any) {
         console.log(err.message);
         isTokenValid = false;
       }
@@ -80,8 +80,8 @@ export default function RootLayout({
               </Title>
               {children}
               <Footer
-                studentId="660610999"
-                fullName="Dome Potikanond"
+                studentId="660612143"
+                fullName="Natchanan Phattarapraditkul"
                 year="2024"
               />
             </Container>
